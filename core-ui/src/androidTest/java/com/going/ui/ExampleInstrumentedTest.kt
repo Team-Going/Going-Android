890066da0626dbf98bbbe3a3f23674b869af2c3e
Sangho Kim <marchbreeze03@gmail.com>
2023-12-27 15:07:47 +0900

    [ADD/#1] base util 추가

--- core-ui/src/androidTest/java/com/going/ui/ExampleInstrumentedTest.kt
+++ core-ui/src/androidTest/java/com/going/ui/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.going.core_ui
+package com.going.ui
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.going.core_ui", appContext.packageName)
+        assertEquals("com.going.ui", appContext.packageName)
     }
 }
--- core-ui/src/test/java/com/going/ui/ExampleUnitTest.kt
+++ core-ui/src/test/java/com/going/ui/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.going.core_ui
+package com.going.ui
 
 import org.junit.Test
 
