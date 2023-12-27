0e279910d1468bec1f386a4c78c97303973ad0bd
Sangho Kim <marchbreeze03@gmail.com>
2023-12-27 16:13:44 +0900

    [ADD/#1] data 모듈 가이드파일 작성

---
+++ data/src/main/java/com/going/data/datasourceImpl/MockDataSourceImpl.kt
@@ -0,0 +1,18 @@
+package com.going.data.datasourceImpl
+
+import com.going.data.datasource.MockDataSource
+import com.going.data.dto.response.MockFollowerResponseDto
+import com.going.data.service.MockService
+import javax.inject.Inject
+
+class MockDataSourceImpl @Inject constructor(
+    private val mockService: MockService
+) : MockDataSource {
+
+    override suspend fun getFollowerListData(
+        page: Int
+    ): MockFollowerResponseDto {
+        return mockService.getFollowerList(page)
+    }
+
+}
