95049f1cc3d9ceead7301da066393f604e51ae28
Dongmin <pdm001125@naver.com>
2023-12-30 13:41:32 +0900

    [ADD/#7] Activity, ViewModel 기초 생성

---
+++ presentation/src/main/java/com/going/presentation/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.going.presentation.auth
+
+import android.os.Bundle
+import com.going.presentation.R
+import com.going.presentation.databinding.ActivityLoginBinding
+import com.going.ui.base.BaseActivity
+
+class LoginActivity() : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ presentation/src/main/java/com/going/presentation/auth/LoginViewModel.kt
@@ -0,0 +1,5 @@
+package com.going.presentation.auth
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel()
