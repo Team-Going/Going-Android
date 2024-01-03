47bd2c467e628ddaeb7ba844ca26262056106d6b
Dongmin <pdm001125@naver.com>
2024-01-03 16:47:58 +0900

    [CHORE/#21] 액티비티 생성

---
+++ presentation/src/main/java/com/going/presentation/tendencytest/TendencyTestActivity.kt
@@ -0,0 +1,13 @@
+package com.going.presentation.tendencytest
+
+import android.os.Bundle
+import com.going.presentation.R
+import com.going.presentation.databinding.ActivityTendencyTestBinding
+import com.going.ui.base.BaseActivity
+
+class TendencyTestActivity :
+    BaseActivity<ActivityTendencyTestBinding>(R.layout.activity_tendency_test) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
