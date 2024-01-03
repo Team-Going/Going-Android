1d27f1a11d8acb5310cad7c9d9cf5091d6b218e7
Dongmin <pdm001125@naver.com>
2024-01-04 03:39:48 +0900

    [FEAT/#21] mock Data를 활용한 항목 이동 기능 구현

--- presentation/src/main/java/com/going/presentation/tendencytest/TendencyTestActivity.kt
+++ presentation/src/main/java/com/going/presentation/tendencytest/TendencyTestActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.viewModels
 import com.going.presentation.R
 import com.going.presentation.databinding.ActivityTendencyTestBinding
 import com.going.ui.base.BaseActivity
+import com.going.ui.extension.setOnSingleClickListener
 
 class TendencyTestActivity :
     BaseActivity<ActivityTendencyTestBinding>(R.layout.activity_tendency_test) {
@@ -15,6 +16,20 @@ class TendencyTestActivity :
         super.onCreate(savedInstanceState)
 
         initBindingViewModel()
+
+        binding.btnTendencyNext.setOnSingleClickListener {
+            if (viewModel.step.value != 9) {
+                viewModel.stepUp()
+                binding.rgAnswers.clearCheck()
+                viewModel.isChecked.value = false
+            } else {
+                // 페이지 이동
+            }
+        }
+
+        binding.rgAnswers.setOnCheckedChangeListener { group, checkedId ->
+            viewModel.isChecked.value = true
+        }
     }
 
     private fun initBindingViewModel() {
--- presentation/src/main/java/com/going/presentation/tendencytest/TendencyTestViewModel.kt
+++ presentation/src/main/java/com/going/presentation/tendencytest/TendencyTestViewModel.kt
@@ -1,5 +1,84 @@
 package com.going.presentation.tendencytest
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.going.domain.entity.TendencyTestMock
 
-class TendencyTestViewModel : ViewModel()
+class TendencyTestViewModel : ViewModel() {
+    val step = MutableLiveData(1)
+    val isChecked = MutableLiveData(false)
+
+    val mockList: List<TendencyTestMock> = listOf(
+        TendencyTestMock(
+            "1번 문항",
+            "1-1",
+            "1-2",
+            "1-3",
+            "1-4",
+        ),
+        TendencyTestMock(
+            "2번 문항",
+            "2-1",
+            "2-2",
+            "2-3",
+            "2-4",
+        ),
+        TendencyTestMock(
+            "3번 문항",
+            "3-1",
+            "3-2",
+            "3-3",
+            "3-4",
+        ),
+        TendencyTestMock(
+            "4번 문항",
+            "4-1",
+            "4-2",
+            "4-3",
+            "4-4",
+        ),
+        TendencyTestMock(
+            "5번 문항",
+            "5-1",
+            "5-2",
+            "5-3",
+            "5-4",
+        ),
+        TendencyTestMock(
+            "6번 문항",
+            "6-1",
+            "6-2",
+            "6-3",
+            "6-4",
+        ),
+        TendencyTestMock(
+            "7번 문항",
+            "7-1",
+            "7-2",
+            "7-3",
+            "7-4",
+        ),
+        TendencyTestMock(
+            "8번 문항",
+            "8-1",
+            "8-2",
+            "8-3",
+            "8-4",
+        ),
+        TendencyTestMock(
+            "9번 문항",
+            "9-1",
+            "9-2",
+            "9-3",
+            "9-4",
+        ),
+    )
+
+    fun stepUp() {
+        step.value = step.value?.plus(1)
+    }
+
+    companion object {
+        const val MAX_STEP = 9
+    }
+}
