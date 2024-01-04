24d75714634563dba3f2a1cf31fd094715633946
Sangho Kim <marchbreeze03@gmail.com>
2024-01-05 03:05:40 +0900

    [UI/#11] OurTodoFragment 설정

--- presentation/src/main/java/com/going/presentation/todo/ourtodo/todolist/OurTodoCompleteFragment.kt
+++ presentation/src/main/java/com/going/presentation/todo/ourtodo/todolist/OurTodoCompleteFragment.kt
@@ -1,11 +1,12 @@
 package com.going.presentation.todo.ourtodo.todolist
 
 import com.going.presentation.R
-import com.going.presentation.databinding.FragmentOurTodoBinding
+import com.going.presentation.databinding.FragmentOurTodoCompleteBinding
 import com.going.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class OurTodoCompleteFragment() : BaseFragment<FragmentOurTodoBinding>(R.layout.fragment_our_todo) {
+class OurTodoCompleteFragment() :
+    BaseFragment<FragmentOurTodoCompleteBinding>(R.layout.fragment_our_todo_complete) {
 
 }
