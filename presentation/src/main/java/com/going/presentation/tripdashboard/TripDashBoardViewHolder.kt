6be8cf8078090c943c647577ba21cd8c461b6f06
LEE YOU BIN <youbeene3409@naver.com>
2024-01-07 18:09:53 +0900

    [UI/#30] 여행 생성 아이템 UI

--- presentation/src/main/java/com/going/presentation/tripdashboard/TripDashBoardViewHolder.kt
+++ presentation/src/main/java/com/going/presentation/tripdashboard/TripDashBoardViewHolder.kt
@@ -1,8 +1,15 @@
 package com.going.presentation.tripdashboard
 
 import androidx.recyclerview.widget.RecyclerView
+import com.going.domain.entity.response.TripCreateListModel
 import com.going.presentation.databinding.ItemDashBoardTripCreateBinding
 
 class TripDashBoardViewHolder(val binding : ItemDashBoardTripCreateBinding) : RecyclerView.ViewHolder(binding.root) {
 
+    fun onBind(item : TripCreateListModel){
+        binding.tvDashboardTripTitle.text = item.title
+        binding.tvDashboardDateStart.text = item.startDate
+        binding.tvDashboardDateEnd.text = item.endDate
+
+    }
 }
