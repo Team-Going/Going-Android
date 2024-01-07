7992b0bb2b3680ffd096625173b25681be8a7859
Sangho Kim <marchbreeze03@gmail.com>
2024-01-08 02:55:36 +0900

    [UI/#33] MyTodo 아이템 설정

--- presentation/src/main/java/com/going/presentation/todo/mytodo/todolist/MyTodoViewPagerAdapter.kt
+++ presentation/src/main/java/com/going/presentation/todo/mytodo/todolist/MyTodoViewPagerAdapter.kt
@@ -2,8 +2,6 @@ package com.going.presentation.todo.mytodo.todolist
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.going.presentation.todo.ourtodo.todolist.OurTodoCompleteFragment
-import com.going.presentation.todo.ourtodo.todolist.OurTodoUncompleteFragment
 
 class MyTodoViewPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
