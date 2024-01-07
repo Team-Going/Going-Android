687db952130da604c7122dc7203511eb6a11b8af
Dongmin <pdm001125@naver.com>
2024-01-07 10:22:43 +0900

    [MOD/#27] toolbar, img, title 작성

---
+++ presentation/src/main/java/com/going/presentation/tendencytest/result/TendencyTestResultActivity.kt
@@ -0,0 +1,8 @@
+package com.going.presentation.tendencytest.result
+
+import com.going.presentation.R
+import com.going.presentation.databinding.ActivityTendencyTestResultBinding
+import com.going.ui.base.BaseActivity
+
+class TendencyTestResultActivity :
+    BaseActivity<ActivityTendencyTestResultBinding>(R.layout.activity_tendency_test_result)
