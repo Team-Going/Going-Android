c82ab8bfdfbd5e172115e117fb93c3ef429589d7
Sangho Kim <marchbreeze03@gmail.com>
2024-01-07 15:47:37 +0900

    [ADD/#31] Todo 이름 리스트 리사이클러뷰 생성

---
+++ presentation/src/main/java/com/going/presentation/todo/name/TodoNameAdapter.kt
@@ -0,0 +1,33 @@
+package com.going.presentation.todo.name
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import com.going.presentation.databinding.ItemTodoNameBinding
+import com.going.ui.extension.ItemDiffCallback
+
+class TodNameAdapter : ListAdapter<String, TodoNameViewHolder>(diffUtil) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoNameViewHolder {
+        val binding: ItemTodoNameBinding =
+            ItemTodoNameBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TodoNameViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TodoNameViewHolder, position: Int) {
+        holder.onBind(getItem(position))
+    }
+
+    fun addList(newItems: List<String>) {
+        val currentItems = currentList.toMutableList()
+        currentItems.addAll(newItems)
+        submitList(currentItems)
+    }
+
+    companion object {
+        private val diffUtil = ItemDiffCallback<String>(
+            onItemsTheSame = { old, new -> old.length == new.length },
+            onContentsTheSame = { old, new -> old == new },
+        )
+    }
+}
---
+++ presentation/src/main/java/com/going/presentation/todo/name/TodoNameViewHolder.kt
@@ -0,0 +1,20 @@
+package com.going.presentation.todo.name
+
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.going.presentation.R
+import com.going.presentation.databinding.ItemTodoNameBinding
+
+class TodoNameViewHolder(val binding: ItemTodoNameBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    private val redColor = ContextCompat.getColor(binding.root.context, R.color.red_500)
+    private val grayColor = ContextCompat.getColor(binding.root.context, R.color.gray_400)
+
+    fun onBind(item: String) {
+        binding.run {
+            tvTodoName.text = item
+            tvTodoName.setTextColor(if (item == "김상호") redColor else grayColor)
+        }
+    }
+
+}
