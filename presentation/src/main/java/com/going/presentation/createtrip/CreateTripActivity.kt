b0cf6a477f8a453e139ccdd3d84c4b0f30fa435a
crownjoe <crownjoe@naver.com>
2024-01-07 14:50:31 +0900

    [FEAT/#26] 여행 생성 UI

---
+++ presentation/src/main/java/com/going/presentation/createtrip/CreateTripActivity.kt
@@ -0,0 +1,12 @@
+package com.going.presentation.createtrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.going.presentation.R
+
+class CreateTripActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_trip)
+    }
+}
