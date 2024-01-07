e4e46597cd71ced695022f1b7e35eb334899af34
Sangho Kim <marchbreeze03@gmail.com>
2024-01-08 03:29:48 +0900

    [FEAT/#33] 나만보기 구분 구현

--- presentation/src/main/java/com/going/presentation/todo/mytodo/MyTodoViewModel.kt
+++ presentation/src/main/java/com/going/presentation/todo/mytodo/MyTodoViewModel.kt
@@ -17,17 +17,18 @@ class MyTodoViewModel : ViewModel() {
     )
 
     val mockUncompleteTodoList: List<TodoModel> = listOf(
-        TodoModel(0,"숙소 예약하기", "2024-01-12", listOf("김상호", "박동민")),
-        TodoModel(1,"기차 왕복 예약하기", "2024-01-14", listOf("조세연")),
-        TodoModel(2,"와사비맛 아몬드 먹기", "2024-01-15", listOf("이유빈", "김상호")),
-        TodoModel(3,"커피 사기", "2024-01-15", listOf("이유빈")),
-        TodoModel(4,"숙소 예약하기", "2024-01-12", listOf("김상호", "박동민")),
-        TodoModel(5,"기차 왕복 예약하기", "2024-01-14", listOf("조세연")),
-        TodoModel(6,"와사비맛 아몬드 먹기", "2024-01-15", listOf("이유빈", "김상호")),
-        TodoModel(7,"커피 사기", "2024-01-15", listOf("이유빈"))
+        TodoModel(0, "숙소 예약하기", "2024-01-12", listOf("김상호", "박동민"), false),
+        TodoModel(1, "기차 왕복 예약하기", "2024-01-14", listOf("조세연"), false),
+        TodoModel(2, "와사비맛 아몬드 먹기", "2024-01-15", listOf("이유빈", "김상호"), false),
+        TodoModel(3, "커피 사기", "2024-01-15", listOf("이유빈"), true),
+        TodoModel(4, "숙소 예약하기", "2024-01-12", listOf("김상호", "박동민"), false),
+        TodoModel(5, "기차 왕복 예약하기", "2024-01-14", listOf("조세연"), false),
+        TodoModel(6, "와사비맛 아몬드 먹기", "2024-01-15", listOf("이유빈", "김상호"), false),
+        TodoModel(7, "커피 사기", "2024-01-15", listOf("이유빈"), true)
     )
 
     val mockCompleteTodoList: List<TodoModel> = listOf(
-        TodoModel(0,"숙소 예약하기", "2024-01-12", listOf("김상호", "박동민")),
+        TodoModel(0, "숙소 예약하기", "2024-01-12", listOf("김상호", "박동민"), false),
+        TodoModel(1, "커피 사기", "2024-01-15", listOf("이유빈"), true),
     )
 }
--- presentation/src/main/java/com/going/presentation/todo/mytodo/todolist/MyTodoListViewHolder.kt
+++ presentation/src/main/java/com/going/presentation/todo/mytodo/todolist/MyTodoListViewHolder.kt
@@ -1,6 +1,7 @@
 package com.going.presentation.todo.mytodo.todolist
 
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.going.domain.entity.response.TodoModel
 import com.going.presentation.R
@@ -16,16 +17,26 @@ class MyTodoListViewHolder(
         binding.run {
             tvMyTodoItemTitle.text = item.title
             tvMyTodoItemDate.text = item.endDate
+
+            layoutMyTodoLock.isVisible = item.secret
+            rvMyTodoName.isVisible = !item.secret
+
             rvMyTodoName.adapter = TodoNameAdapter(isCompleted).apply {
                 submitList(item.allocation)
             }
+
             if (isCompleted) {
                 tvMyTodoItemTitle.setTextColor(ContextCompat.getColor(binding.root.context,R.color.gray_300))
                 tvMyTodoItemDate.setTextColor(ContextCompat.getColor(binding.root.context,R.color.gray_200))
+                layoutMyTodoLock.setBackgroundResource(R.drawable.shape_rect_2_gray300_line)
             } else {
                 tvMyTodoItemTitle.setTextColor(ContextCompat.getColor(binding.root.context,R.color.black_000))
                 tvMyTodoItemDate.setTextColor(ContextCompat.getColor(binding.root.context,R.color.gray_300))
+                layoutMyTodoLock.setBackgroundResource(R.drawable.shape_rect_2_gray400_line)
             }
+
+            cbMyTodoSelected.isVisible = isCompleted
+            cbMyTodoUnselected.isVisible = !isCompleted
         }
     }
 
