25fd34e1e4e69ae7434d114181e85c3e248b1a8b
Dongmin <pdm001125@naver.com>
2024-01-07 18:00:29 +0900

    [CHORE/#27] 함수화

--- presentation/src/main/java/com/going/presentation/tendencytest/result/TendencyTestResultActivity.kt
+++ presentation/src/main/java/com/going/presentation/tendencytest/result/TendencyTestResultActivity.kt
@@ -16,23 +16,34 @@ class TendencyTestResultActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.tvFirstDescriptionTitle.text = viewModel.mockList[0].title
-        binding.tvFirstDescriptionFirstText.text = test(viewModel.mockList[0].first)
-        binding.tvFirstDescriptionSecondText.text = test(viewModel.mockList[0].second)
-        binding.tvFirstDescriptionThirdText.text = test(viewModel.mockList[0].third)
-
-        binding.tvSecondDescriptionTitle.text = viewModel.mockList[1].title
-        binding.tvSecondDescriptionFirstText.text = test(viewModel.mockList[1].first)
-        binding.tvSecondDescriptionSecondText.text = test(viewModel.mockList[1].second)
-        binding.tvSecondDescriptionThirdText.text = test(viewModel.mockList[1].third)
-
-        binding.tvThirdDescriptionTitle.text = viewModel.mockList[2].title
-        binding.tvThirdDescriptionFirstText.text = test(viewModel.mockList[2].first)
-        binding.tvThirdDescriptionSecondText.text = test(viewModel.mockList[2].second)
-        binding.tvThirdDescriptionThirdText.text = test(viewModel.mockList[2].third)
+        bindTendencyInfo()
 
         initFinishBtnClickListener()
         initRestartBtnClickListener()
+    }
+
+    private fun bindTendencyInfo() {
+        binding.tvFirstDescriptionTitle.text = viewModel.mockList[0].title
+        binding.tvFirstDescriptionFirstText.text = setBulletPoint(viewModel.mockList[0].first)
+        binding.tvFirstDescriptionSecondText.text = setBulletPoint(viewModel.mockList[0].second)
+        binding.tvFirstDescriptionThirdText.text = setBulletPoint(viewModel.mockList[0].third)
+
+        binding.tvSecondDescriptionTitle.text = viewModel.mockList[1].title
+        binding.tvSecondDescriptionFirstText.text = setBulletPoint(viewModel.mockList[1].first)
+        binding.tvSecondDescriptionSecondText.text = setBulletPoint(viewModel.mockList[1].second)
+        binding.tvSecondDescriptionThirdText.text = setBulletPoint(viewModel.mockList[1].third)
+
+        binding.tvThirdDescriptionTitle.text = viewModel.mockList[2].title
+        binding.tvThirdDescriptionFirstText.text = setBulletPoint(viewModel.mockList[2].first)
+        binding.tvThirdDescriptionSecondText.text = setBulletPoint(viewModel.mockList[2].second)
+        binding.tvThirdDescriptionThirdText.text = setBulletPoint(viewModel.mockList[2].third)
+    }
+
+    private fun setBulletPoint(text: String): SpannableString {
+        val string = SpannableString(text)
+        string.setSpan(BulletSpan(), 0, text.length - 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        return string
     }
 
     private fun initFinishBtnClickListener() {
@@ -45,12 +56,5 @@ class TendencyTestResultActivity :
         binding.btnTendencyTestResultRestart.setOnSingleClickListener {
             // 다시 시작
         }
-    }
-
-    fun test(text: String): SpannableString {
-        val string = SpannableString(text)
-        string.setSpan(BulletSpan(), 0, text.length - 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-        return string
     }
 }
