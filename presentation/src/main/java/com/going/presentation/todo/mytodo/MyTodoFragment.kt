81b2d4396af6b49a04ab0b4e7b16ca379d4a99f6
Sangho Kim <marchbreeze03@gmail.com>
2024-01-08 02:14:38 +0900

    [UI/#33] MyTodo 레이아웃 구현

--- presentation/src/main/java/com/going/presentation/todo/mytodo/MyTodoFragment.kt
+++ presentation/src/main/java/com/going/presentation/todo/mytodo/MyTodoFragment.kt
@@ -1,18 +1,65 @@
 package com.going.presentation.todo.mytodo
 
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.going.presentation.R
 import com.going.presentation.databinding.FragmentMyTodoBinding
+import com.going.presentation.todo.ourtodo.todolist.OurTodoViewPagerAdapter
 import com.going.ui.base.BaseFragment
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MyTodoFragment() : BaseFragment<FragmentMyTodoBinding>(R.layout.fragment_my_todo) {
 
+    private val tabTextList = listOf(TAB_UNCOMPLETE, TAB_COMPLETE)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setDateTextColor()
+        setTabLayout()
+        setViewPager()
+    }
+
+    private fun setDateTextColor() {
+        binding.tvMyTodoTitleDown.apply {
+            text = SpannableStringBuilder(text).apply {
+                setSpan(
+                    ForegroundColorSpan(
+                        ContextCompat.getColor(
+                            requireContext(), R.color.red_500
+                        )
+                    ), 3, length - 4, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+            }
+        }
+    }
+
+    private fun setTabLayout() {
+        binding.tabMyTodo.apply {
+            for (tabName in tabTextList) {
+                val tab = this.newTab()
+                tab.text = tabName
+                this.addTab(tab)
+            }
+        }
+    }
+
+    private fun setViewPager() {
+        binding.vpMyTodo.adapter = OurTodoViewPagerAdapter(this)
+        TabLayoutMediator(binding.tabMyTodo, binding.vpMyTodo) { tab, pos ->
+            tab.text = tabTextList[pos]
+        }.attach()
+    }
+
+    companion object {
+        const val TAB_UNCOMPLETE = "미완료 todo"
+        const val TAB_COMPLETE = "완료 todo"
     }
 
 }
