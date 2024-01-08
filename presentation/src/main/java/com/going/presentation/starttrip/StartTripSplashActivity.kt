ec13ec00cb06f75ab9765b3ee2076050ee0c9647
crownjoe <crownjoe@naver.com>
2024-01-08 23:34:27 +0900

    [FEAT/#37] splash 구현 완료

--- presentation/src/main/java/com/going/presentation/starttrip/StartTripSplashActivity.kt
+++ presentation/src/main/java/com/going/presentation/starttrip/StartTripSplashActivity.kt
@@ -1,12 +1,34 @@
 package com.going.presentation.starttrip
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import com.going.presentation.R
+import com.going.presentation.databinding.ActivityStartTripSplashBinding
+import com.going.presentation.starttrip.createtrip.CreateTripActivity
+import com.going.ui.base.BaseActivity
+import com.going.ui.extension.setOnSingleClickListener
 
-class StartTripSplashActivity : AppCompatActivity() {
+class StartTripSplashActivity :
+    BaseActivity<ActivityStartTripSplashBinding>(R.layout.activity_start_trip_splash) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start_trip_splash)
+
+        initNewTripBtnClickListener()
+        initEnterTripBtnClickListener()
+    }
+
+    private fun initNewTripBtnClickListener() {
+        binding.btnStartTripSplashNewTrip.setOnSingleClickListener {
+            Intent(this, CreateTripActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
+        finish()
+    }
+
+    private fun initEnterTripBtnClickListener() {
+        binding.btnStartTripSplashEnterTrip.setOnSingleClickListener {
+        //초대 코드 받아서 여행 입장하는 액티비티로 연결
+        }
     }
 }
