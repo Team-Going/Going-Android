6299c0ea84be0c9a698da58cc89ca5d640b0b794
Sangho Kim <marchbreeze03@gmail.com>
2024-01-09 01:42:36 +0900

    [UI/#45] 마이투두생성 툴바 설정

---
+++ presentation/src/main/java/com/going/presentation/todo/mytodo/create/MyTodoCreateActivity.kt
@@ -0,0 +1,17 @@
+package com.going.presentation.todo.mytodo.create
+
+import android.os.Bundle
+import com.going.presentation.R
+import com.going.presentation.databinding.ActivityMyTodoCreateBinding
+import com.going.ui.base.BaseActivity
+
+class MyTodoCreateActivity :
+    BaseActivity<ActivityMyTodoCreateBinding>(R.layout.activity_my_todo_create) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+}
