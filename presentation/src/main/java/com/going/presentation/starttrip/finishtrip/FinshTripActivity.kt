1c3aa02dcfab7c266f4d0c0173b2f1947327623b
crownjoe <crownjoe@naver.com>
2024-01-09 06:23:35 +0900

    [FEAT/#47] finish trip xml 구현

---
+++ presentation/src/main/java/com/going/presentation/starttrip/finishtrip/FinshTripActivity.kt
@@ -0,0 +1,12 @@
+package com.going.presentation.starttrip.finishtrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.going.presentation.R
+
+class FinshTripActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finsh_trip)
+    }
+}
