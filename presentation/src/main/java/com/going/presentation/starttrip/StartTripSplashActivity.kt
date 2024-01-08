aca089f941087d9cc79da4aaef29557328cb67e9
crownjoe <crownjoe@naver.com>
2024-01-08 23:18:21 +0900

    [FEAT/#37] starttrip splash xml

---
+++ presentation/src/main/java/com/going/presentation/starttrip/StartTripSplashActivity.kt
@@ -0,0 +1,12 @@
+package com.going.presentation.starttrip
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.going.presentation.R
+
+class StartTripSplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_trip_splash)
+    }
+}
