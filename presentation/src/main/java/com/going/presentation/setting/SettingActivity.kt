5cd269b4cd22aaee1f8d24d85a2fe84d44afb00f
LEE YOU BIN <youbeene3409@naver.com>
2024-01-10 01:56:38 +0900

    [FEAT/#51] 버튼 클릭 리스너 구현

--- presentation/src/main/java/com/going/presentation/setting/SettingActivity.kt
+++ presentation/src/main/java/com/going/presentation/setting/SettingActivity.kt
@@ -4,11 +4,57 @@ import android.os.Bundle
 import com.going.presentation.R
 import com.going.presentation.databinding.ActivitySettingBinding
 import com.going.ui.base.BaseActivity
+import com.going.ui.extension.setOnSingleClickListener
+import com.skydoves.balloon.balloon
 
 class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_setting) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initProfileClickListener()
+        initInquireClickListener()
+        initServiceVersionClickListener()
+        initPolicyClickListener()
+        initAboutDoorip()
+        initLogoutClickListener()
+
+    }
+
+    private fun initProfileClickListener(){
+        binding.btnSettingProfileEnter.setOnSingleClickListener {
+
+        }
+    }
+
+    private fun initInquireClickListener(){
+        binding.btnSettingInquireEnter.setOnSingleClickListener {
+
+        }
+    }
+
+    private fun initServiceVersionClickListener(){
+        binding.btnSettingServiceVersionEnter.setOnSingleClickListener {
+
+        }
+    }
+
+    private fun initPolicyClickListener(){
+        binding.btnSettingPolicyEnter.setOnSingleClickListener {
+
+        }
+    }
+
+    private fun initAboutDoorip(){
+        binding.btnSettingAboutDooripEnter.setOnSingleClickListener {
+
+        }
+    }
+
+    private fun initLogoutClickListener(){
+        binding.btnSettingLogoutEnter.setOnSingleClickListener {
+
+        }
     }
 
 }
