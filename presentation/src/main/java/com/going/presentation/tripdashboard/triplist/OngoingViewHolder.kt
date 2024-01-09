974919481dbaca49b11a6c8c9fbfc21856931654
LEE YOU BIN <youbeene3409@naver.com>
2024-01-09 23:21:21 +0900

    [FIX/#44] String 값 추출

--- presentation/src/main/java/com/going/presentation/tripdashboard/triplist/OngoingViewHolder.kt
+++ presentation/src/main/java/com/going/presentation/tripdashboard/triplist/OngoingViewHolder.kt
@@ -1,8 +1,8 @@
 package com.going.presentation.tripdashboard.triplist
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.going.domain.entity.response.OngoingListModel
+import com.going.presentation.R
 import com.going.presentation.databinding.ItemDashBoardOngoingBinding
 import com.going.ui.extension.setOnSingleClickListener
 
@@ -12,18 +12,18 @@ class OngoingViewHolder(
 ) : RecyclerView.ViewHolder(binding.root) {
 
     fun onBind(item: OngoingListModel) {
-
         binding.run {
             tvDashboardTripTitle.text = item.title
             tvDashboardDateStart.text = item.startDate
             tvDashboardDateEnd.text = item.endDate
 
-//            if (item.day <= 0) {
-//                layoutDashboardTraveling.visibility = View.VISIBLE
-//                layoutDashboardDayLeft.visibility = View.INVISIBLE
-//            } else {
-//                tvDashboardDeadlineDay.text = item.day.toString()
-//            }
+            if (item.day <= 0) {
+                tvDashboardDeadline.text =
+                    itemView.context.getString(R.string.dashboard_tv_traveling)
+            } else {
+                tvDashboardDeadline.text =
+                    itemView.context.getString(R.string.dashboard_tv_deadline, item.day)
+            }
 
             layoutDashboard.setOnSingleClickListener {
                 listener.onDashBoardSelectedListener(item)
