098cb9cedf8f824878de828623f76ff65bea28db
Dongmin <pdm001125@naver.com>
2024-01-10 00:05:17 +0900

    [CHORE/#41] 줄바꿈

--- domain/src/main/kotlin/com/going/domain/repository/TokenRepository.kt
+++ domain/src/main/kotlin/com/going/domain/repository/TokenRepository.kt
@@ -2,7 +2,8 @@ package com.going.domain.repository
 
 interface TokenRepository {
     fun getAccessToken(): String
-    fun setTokens(accessToken: String, refreshToken: String)
 
     fun getRefreshToken(): String
+
+    fun setTokens(accessToken: String, refreshToken: String)
 }
