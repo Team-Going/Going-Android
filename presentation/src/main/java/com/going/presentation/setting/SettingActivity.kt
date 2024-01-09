f2edfb10cfd94d49979fc34c5061aff7a5ad44e9
LEE YOU BIN <youbeene3409@naver.com>
2024-01-10 00:11:22 +0900

    [ADD/#51] 설정 파일 추가

---
+++ presentation/src/main/java/com/going/presentation/setting/SettingActivity.kt
@@ -0,0 +1,14 @@
+package com.going.presentation.setting
+
+import android.os.Bundle
+import com.going.presentation.R
+import com.going.presentation.databinding.ActivitySettingBinding
+import com.going.ui.base.BaseActivity
+
+class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
