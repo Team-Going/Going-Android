761b71676cab86cdb290795e4ec69e342d574f7a
Sangho Kim <marchbreeze03@gmail.com>
2024-01-10 00:27:55 +0900

    [ADD/#54] 마이투두 생성 xml 쇽샥

---
+++ presentation/src/main/java/com/going/presentation/todo/mytodo/detail/MyTodoDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.going.presentation.todo.mytodo.detail
+
+import com.going.presentation.R
+import com.going.presentation.databinding.ActivityMyTodoDetailBinding
+import com.going.ui.base.BaseActivity
+
+class MyTodoCreateActivity :
+    BaseActivity<ActivityMyTodoDetailBinding>(R.layout.activity_my_todo_detail) {
+
+
+}
