386b29e6b0851bf4d061d151cd36369ddc6ea11e
Dongmin <pdm001125@naver.com>
2024-01-09 17:35:13 +0900

    [FIX/#41] 서버통신 에러코드 익스텐션 간략화

--- presentation/src/main/java/com/going/presentation/util/JsonExt.kt
+++ presentation/src/main/java/com/going/presentation/util/JsonExt.kt
@@ -3,16 +3,10 @@ package com.going.presentation.util
 import org.json.JSONObject
 import retrofit2.HttpException
 
-fun toErrorCode(throwable: Throwable): String {
-    lateinit var errorCode: String
-
-    if (throwable is HttpException) {
-        val jsonTemp = throwable.response()?.errorBody()?.byteString().toString()
-        val json = jsonTemp.slice(6 until jsonTemp.length)
-        errorCode = JSONObject(json).getString("code")
-    } else {
-        errorCode = "EXCEPTION"
-    }
-
-    return errorCode
+fun toErrorCode(throwable: Throwable): String = if (throwable is HttpException) {
+    val jsonTemp = throwable.response()?.errorBody()?.byteString().toString()
+    val json = jsonTemp.slice(6 until jsonTemp.length)
+    JSONObject(json).getString("code")
+} else {
+    "EXCEPTION"
 }
