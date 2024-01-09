8282175f0c193b0bf2487b2d7c3262894f3cc8bc
crownjoe <crownjoe@naver.com>
2024-01-09 23:24:32 +0900

    [FEAT/#50] profile 초기 커밋

---
+++ presentation/src/main/java/com/going/presentation/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.going.presentation.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.going.presentation.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
