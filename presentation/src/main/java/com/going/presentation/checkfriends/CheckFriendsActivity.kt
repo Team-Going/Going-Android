478ea29f34e7a05766bd0ceca745b957c00143c1
LEE YOU BIN <youbeene3409@naver.com>
2024-01-10 22:52:09 +0900

    [ADD/#66] 여행 친구들 조회 파일 추가

---
+++ presentation/src/main/java/com/going/presentation/checkfriends/CheckFriendsActivity.kt
@@ -0,0 +1,15 @@
+package com.going.presentation.checkfriends
+
+import android.os.Bundle
+import com.going.presentation.R
+import com.going.presentation.databinding.ActivityCheckFriendsBinding
+import com.going.ui.base.BaseActivity
+
+class CheckFriendsActivity :
+    BaseActivity<ActivityCheckFriendsBinding>(R.layout.activity_check_friends) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
