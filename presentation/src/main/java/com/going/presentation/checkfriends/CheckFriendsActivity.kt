e12a0e49084df81f30a40ee559a347bfe7f32516
LEE YOU BIN <youbeene3409@naver.com>
2024-01-11 00:21:56 +0900

    [FEAT/#66] 취향 태그 String 추출 및 UI

--- presentation/src/main/java/com/going/presentation/checkfriends/CheckFriendsActivity.kt
+++ presentation/src/main/java/com/going/presentation/checkfriends/CheckFriendsActivity.kt
@@ -4,12 +4,24 @@ import android.os.Bundle
 import com.going.presentation.R
 import com.going.presentation.databinding.ActivityCheckFriendsBinding
 import com.going.ui.base.BaseActivity
+import com.going.ui.extension.setOnSingleClickListener
 
 class CheckFriendsActivity :
     BaseActivity<ActivityCheckFriendsBinding>(R.layout.activity_check_friends) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initBackClickListener()
+
     }
+
+    private fun initBackClickListener(){
+        binding.btnCheckFriendsBack.setOnSingleClickListener {
+            finish()
+        }
+    }
+
+
 
 }
