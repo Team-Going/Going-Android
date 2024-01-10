e678a90acd8202b1c0d3bd6b4860db0388321dbd
LEE YOU BIN <youbeene3409@naver.com>
2024-01-10 17:49:27 +0900

    [MOD/#51] 코드 수정

--- presentation/src/main/java/com/going/presentation/setting/SettingActivity.kt
+++ presentation/src/main/java/com/going/presentation/setting/SettingActivity.kt
@@ -1,16 +1,11 @@
 package com.going.presentation.setting
 
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.going.presentation.R
 import com.going.presentation.databinding.ActivitySettingBinding
 import com.going.ui.base.BaseActivity
 import com.going.ui.extension.setOnSingleClickListener
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_setting) {
 
@@ -54,6 +49,12 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_s
 
     private fun initLogoutClickListener() {
         binding.btnSettingLogoutEnter.setOnSingleClickListener {
+
+        }
+    }
+
+    private fun initQuitClickListener() {
+        binding.btnSettingQuit.setOnSingleClickListener {
             showLogoutAlertDialog()
         }
     }
@@ -61,17 +62,7 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_s
     private fun showLogoutAlertDialog() {
         settingDialog = SettingCustomDialogFragment()
         settingDialog.show(supportFragmentManager, settingDialog.tag)
-        val scope = CoroutineScope(Dispatchers.Main)
-        scope.launch {
-            delay(190)
-            window.statusBarColor = ContextCompat.getColor(this@SettingActivity, R.color.transparent_60)
-        }
-    }
-
-   private fun initQuitClickListener(){
-        binding.btnSettingQuit.setOnSingleClickListener {
-
-        }
+        window.statusBarColor = ContextCompat.getColor(this@SettingActivity, R.color.transparent_60)
     }
 
 }
--- presentation/src/main/java/com/going/presentation/setting/SettingCustomDialogFragment.kt
+++ presentation/src/main/java/com/going/presentation/setting/SettingCustomDialogFragment.kt
@@ -5,21 +5,19 @@ import android.view.View
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
 import com.going.presentation.R
-import com.going.presentation.databinding.FragmentCustomDialogSettingBinding
+import com.going.presentation.databinding.FragmentSettingCustomDialogBinding
 import com.going.ui.base.BaseDialog
 import com.going.ui.extension.setOnSingleClickListener
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class SettingCustomDialogFragment :
-    BaseDialog<FragmentCustomDialogSettingBinding>(R.layout.fragment_custom_dialog_setting) {
+    BaseDialog<FragmentSettingCustomDialogBinding>(R.layout.fragment_setting_custom_dialog) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         initNegativeClickListener()
         initPositiveClickListener()
+
     }
 
     override fun onStart() {
@@ -35,18 +33,14 @@ class SettingCustomDialogFragment :
 
     private fun initPositiveClickListener() {
         binding.tvDialogPositive.setOnSingleClickListener {
-            val scopeDialog = CoroutineScope(Dispatchers.Main)
-            scopeDialog.launch {
-                delay(500)
-                activity?.window?.statusBarColor =
-                    ContextCompat.getColor(requireContext(), R.color.white_000)
-                dismiss()
-            }
+            activity?.window?.statusBarColor =
+                ContextCompat.getColor(requireContext(), R.color.white_000)
+            dismiss()
         }
     }
 
     private fun initNegativeClickListener() {
-
+        // 탈퇴하기 버튼을 눌렀을 때의 처리
     }
 
 }
