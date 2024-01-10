2de1580a3fc70024902ab05a5672232e5fb4fcdd
crownjoe <crownjoe@naver.com>
2024-01-10 20:27:31 +0900

    [FEAT/#61] enter trip ui 구현

---
+++ presentation/src/main/java/com/going/presentation/entertrip/EnterTripActivity.kt
@@ -0,0 +1,87 @@
+package com.going.presentation.entertrip
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.core.content.res.ResourcesCompat
+import com.going.domain.entity.NameState
+import com.going.presentation.R
+import com.going.presentation.databinding.ActivityEnterTripBinding
+import com.going.presentation.starttrip.StartTripSplashActivity
+import com.going.ui.base.BaseActivity
+import com.going.ui.extension.setOnSingleClickListener
+
+class EnterTripActivity :
+    BaseActivity<ActivityEnterTripBinding>(R.layout.activity_enter_trip) {
+    private val viewModel by viewModels<EnterTripViewModel>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBackBtnClickListener()
+        initBindingViewModel()
+        observeIsCodeAvailable()
+        initNextBtnClickListener()
+
+    }
+
+    private fun initBackBtnClickListener(){
+        binding.btnEnterBack.setOnSingleClickListener{
+            Intent(this, StartTripSplashActivity::class.java).apply {
+                startActivity(this)
+            }
+            finish()
+        }
+    }
+
+    private fun initBindingViewModel() {
+        binding.viewModel = viewModel
+    }
+
+    private fun observeIsCodeAvailable() {
+        viewModel.isCodeAvailable.observe(this) { state ->
+            setColors(
+                false,
+                viewModel.codeLength.value ?: 0,
+                binding.tvCodeCounter,
+            ) { background ->
+                binding.etEnterTripName.background = ResourcesCompat.getDrawable(
+                    this.resources,
+                    background,
+                    theme,
+                )
+            }
+        }
+    }
+
+    private fun setColors(
+        hasFocus: Boolean,
+        length: Int,
+        counter: TextView,
+        setBackground: (Int) -> Unit,
+    ) {
+        val (color, background) = when {
+            viewModel.isCodeAvailable.value != NameState.Blank && hasFocus -> R.color.gray_700 to R.drawable.shape_rect_4_gray700_line
+            length == 0 -> R.color.gray_200 to R.drawable.shape_rect_4_gray200_line
+            viewModel.isCodeAvailable.value == NameState.Blank && counter == binding.tvCodeCounter -> R.color.red_500 to R.drawable.shape_rect_4_red500_line
+            else -> R.color.gray_700 to R.drawable.shape_rect_4_gray700_line
+        }
+        setCounterColor(counter, color)
+        setBackground(background)
+    }
+
+    private fun setCounterColor(counter: TextView, color: Int) {
+        counter.setTextColor(getColor(color))
+    }
+
+    private fun initNextBtnClickListener() {
+        binding.btnEnterTripNext.setOnSingleClickListener {
+            finish()
+        }
+    }
+
+
+
+}
---
+++ presentation/src/main/java/com/going/presentation/entertrip/EnterTripViewModel.kt
@@ -0,0 +1,47 @@
+package com.going.presentation.entertrip
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.going.domain.entity.NameState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class EnterTripViewModel : ViewModel(){
+
+    val inviteCode = MutableLiveData<String>()
+    val codeLength = MutableLiveData(0)
+
+    val isCodeAvailable = MutableLiveData<NameState>(NameState.Empty)
+    var isCheckEnterAvailable = MutableLiveData(false)
+
+    private val _ButtonAvailable = MutableStateFlow(false)
+    val ButtonAvailable: StateFlow<Boolean> = _ButtonAvailable
+
+    fun checkCodeAvailable() {
+        codeLength.value = getNameLength(inviteCode.value)
+
+        isCodeAvailable.value = when { //서버에서 받은 초대코드와 비교
+            codeLength.value == 0 -> NameState.Empty
+            inviteCode.value.isNullOrBlank() -> NameState.Blank
+            else -> NameState.Success.also { checkEnterAvailable() }
+        }
+    }
+
+    private fun getNameLength(value: String?): Int {
+        return value?.length ?: 0
+    }
+
+    fun checkEnterAvailable() {
+        isCheckEnterAvailable.value = isCodeAvailable.value == NameState.Success
+    }
+
+
+
+    fun setButtonAvailable() {
+        _ButtonAvailable.value = true
+    }
+
+    companion object {
+        const val MAX_INVITE_LEN = 6
+    }
+}
