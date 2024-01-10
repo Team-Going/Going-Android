b25b2da533efce3a21653b946fed63c934bdbbc9
Sangho Kim <marchbreeze03@gmail.com>
2024-01-10 09:41:17 +0900

    [ADD/#60] 아워투두 생성 뷰 설정

--- presentation/src/main/java/com/going/presentation/todo/ourtodo/create/OurTodoCreateActivity.kt
+++ presentation/src/main/java/com/going/presentation/todo/ourtodo/create/OurTodoCreateActivity.kt
@@ -1,2 +1,17 @@
 package com.going.presentation.todo.ourtodo.create
 
+import android.os.Bundle
+import com.going.presentation.R
+import com.going.presentation.databinding.ActivityOurTodoCreateBinding
+import com.going.ui.base.BaseActivity
+
+class OurTodoCreateActivity :
+    BaseActivity<ActivityOurTodoCreateBinding>(R.layout.activity_our_todo_create) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+}
