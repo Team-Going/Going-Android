1b663b2616bd0bbe8eda16ad0a908d49fd61e968
Sangho Kim <marchbreeze03@gmail.com>
2024-01-10 10:13:31 +0900

    [FEAT/#60] 마이투두 생성 뷰 쇽샥

--- presentation/src/main/java/com/going/presentation/todo/ourtodo/create/OurTodoCreateActivity.kt
+++ presentation/src/main/java/com/going/presentation/todo/ourtodo/create/OurTodoCreateActivity.kt
@@ -1,17 +1,168 @@
 package com.going.presentation.todo.ourtodo.create
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.core.content.res.ResourcesCompat
 import com.going.presentation.R
 import com.going.presentation.databinding.ActivityOurTodoCreateBinding
+import com.going.presentation.todo.mytodo.create.MyTodoCreateBottomSheet
 import com.going.ui.base.BaseActivity
+import com.going.ui.extension.setOnSingleClickListener
 
 class OurTodoCreateActivity :
     BaseActivity<ActivityOurTodoCreateBinding>(R.layout.activity_our_todo_create) {
 
+    private val viewModel by viewModels<OurTodoCreateViewModel>()
+
+    private var ourTodoCreateBottomSheet: OurTodoCreateBottomSheet? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        initViewModel()
+        initTodoFocusListener()
+        initMemoFocusListener()
+        initDateClickListener()
+        initFinishBtnListener()
+        initBackBtnListener()
+        observeTextLength()
+        observeMemoLength()
+        observeDateEmpty()
+    }
 
+    private fun initViewModel() {
+        binding.vm = viewModel
+    }
+
+    private fun initTodoFocusListener() {
+        binding.etOurTodoCreateTodo.setOnFocusChangeListener { _, hasFocus ->
+            setColors(
+                hasFocus,
+                viewModel.nowTodoLength.value ?: 0,
+                binding.tvOurTodoTodoCounter,
+            ) { background ->
+                binding.etOurTodoCreateTodo.background = setBackgroundColor(background)
+            }
+        }
+    }
+
+    private fun initMemoFocusListener() {
+        binding.etOurTodoCreateMemo.setOnFocusChangeListener { _, hasFocus ->
+            setColors(
+                hasFocus,
+                viewModel.nowMemoLength.value ?: 0,
+                binding.tvOurTodoMemoCounter,
+            ) { background ->
+                binding.etOurTodoCreateMemo.background = setBackgroundColor(background)
+            }
+        }
+    }
+
+    private fun initDateClickListener() {
+        binding.etOurTodoCreateDate.setOnSingleClickListener {
+            ourTodoCreateBottomSheet = MyTodoCreateBottomSheet()
+            ourTodoCreateBottomSheet?.show(supportFragmentManager, DATE_BOTTOM_SHEET)
+        }
+    }
+
+    private fun initFinishBtnListener() {
+        binding.btnOurTodoMemoFinish.setOnSingleClickListener {
+            // 서버통신 진행
+            finish()
+        }
+    }
+
+    private fun initBackBtnListener() {
+        binding.btnOurTodoCreateBack.setOnSingleClickListener {
+            finish()
+        }
+    }
+
+    private fun observeTextLength() {
+        viewModel.nowTodoLength.observe(this) { length ->
+            val maxTodoLen = viewModel.getMaxTodoLen()
+
+            if (length > maxTodoLen) {
+                binding.etOurTodoCreateTodo.apply {
+                    setText(text?.subSequence(0, maxTodoLen))
+                    setSelection(maxTodoLen)
+                }
+            }
+            setColors(
+                false,
+                viewModel.nowTodoLength.value ?: 0,
+                binding.tvOurTodoTodoCounter,
+            ) { background ->
+                binding.etOurTodoCreateTodo.background = setBackgroundColor(background)
+            }
+        }
+    }
+
+    private fun observeMemoLength() {
+        viewModel.nowMemoLength.observe(this) { length ->
+            val maxMemoLen = viewModel.getMaxMemoLen()
+            if (length > maxMemoLen) {
+                binding.etOurTodoCreateTodo.apply {
+                    setText(text?.subSequence(0, maxMemoLen))
+                    setSelection(maxMemoLen)
+                }
+            }
+            setColors(
+                false,
+                viewModel.nowMemoLength.value ?: 0,
+                binding.tvOurTodoMemoCounter,
+            ) { background ->
+                binding.etOurTodoCreateMemo.background = setBackgroundColor(background)
+            }
+        }
+    }
+
+    private fun observeDateEmpty() {
+        viewModel.endDate.observe(this) { text ->
+            if (text.isEmpty()) {
+                binding.etOurTodoCreateDate.setBackgroundResource(R.drawable.shape_rect_4_gray200_line)
+            } else {
+                binding.etOurTodoCreateDate.setBackgroundResource(R.drawable.shape_rect_4_gray700_line)
+            }
+        }
+    }
+
+    private fun setColors(
+        hasFocus: Boolean,
+        length: Int,
+        counter: TextView,
+        setBackground: (Int) -> Unit,
+    ) {
+        val (color, background) = when {
+            hasFocus || viewModel.nowTodoLength.value != 0 -> R.color.gray_700 to R.drawable.shape_rect_4_gray700_line
+            length == 0 -> R.color.gray_200 to R.drawable.shape_rect_4_gray200_line
+            else -> R.color.gray_700 to R.drawable.shape_rect_4_gray700_line
+        }
+        setCounterColor(counter, color)
+        setBackground(background)
+    }
+
+    private fun setCounterColor(counter: TextView, color: Int) {
+        counter.setTextColor(getColor(color))
+    }
+
+    private fun setBackgroundColor(background: Int): Drawable? {
+        return ResourcesCompat.getDrawable(
+            this.resources,
+            background,
+            theme,
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (ourTodoCreateBottomSheet?.isAdded == true) ourTodoCreateBottomSheet?.dismiss()
+    }
+
+    companion object {
+        private const val DATE_BOTTOM_SHEET = "DATE_BOTTOM_SHEET"
     }
 
 }
