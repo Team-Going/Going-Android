c22f97f88cf0d6ee43e210f30b68074ff913adfc
Sangho Kim <marchbreeze03@gmail.com>
2024-01-10 10:07:19 +0900

    [ADD/#60] 아워투두 데이터바인딩 설정

--- presentation/src/main/java/com/going/presentation/todo/ourtodo/create/OurTodoCreateViewModel.kt
+++ presentation/src/main/java/com/going/presentation/todo/ourtodo/create/OurTodoCreateViewModel.kt
@@ -1,2 +1,47 @@
 package com.going.presentation.todo.ourtodo.create
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.text.BreakIterator
+
+class OurTodoCreateViewModel : ViewModel() {
+
+    val todo = MutableLiveData("")
+    val nowTodoLength = MutableLiveData(0)
+
+    val endDate = MutableLiveData("")
+
+    val memo = MutableLiveData("")
+    val nowMemoLength = MutableLiveData(0)
+
+    val isFinishAvailable = MutableLiveData(false)
+
+    fun getMaxTodoLen() = MAX_TODO_LEN
+
+    fun getMaxMemoLen() = MAX_MEMO_LEN
+
+    fun checkIsFinishAvailable() {
+        nowTodoLength.value = getGraphemeLength(todo.value)
+        nowMemoLength.value = getGraphemeLength(memo.value)
+        isFinishAvailable.value =
+            todo.value?.isNotEmpty() == true && memo.value?.isNotEmpty() == true && endDate.value?.isNotEmpty() == true
+    }
+
+    // 이모지 포함 글자 수 세는 함수
+    private fun getGraphemeLength(value: String?): Int {
+        BREAK_ITERATOR.setText(value)
+        var count = 0
+        while (BREAK_ITERATOR.next() != BreakIterator.DONE) {
+            count++
+        }
+        return count
+    }
+
+    companion object {
+        val BREAK_ITERATOR: BreakIterator = BreakIterator.getCharacterInstance()
+
+        const val MAX_TODO_LEN = 15
+        const val MAX_MEMO_LEN = 1000
+    }
+
+}
