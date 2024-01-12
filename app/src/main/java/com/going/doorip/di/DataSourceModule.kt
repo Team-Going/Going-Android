97e27144c61a02656cb365658d7fb37aa9ed565e
crownjoe <crownjoe@naver.com>
2024-01-12 09:27:15 +0900

    [FEAT/#77] enter trip di 수정

--- app/src/main/java/com/going/doorip/di/DataSourceModule.kt
+++ app/src/main/java/com/going/doorip/di/DataSourceModule.kt
@@ -1,10 +1,12 @@
 package com.going.doorip.di
 
 import com.going.data.datasource.AuthDataSource
+import com.going.data.datasource.EnterTripDataSource
 import com.going.data.datasource.MockDataSource
 import com.going.data.datasource.SettingDataSource
 import com.going.data.datasource.TodoDataSource
 import com.going.data.datasourceImpl.AuthDataSourceImpl
+import com.going.data.datasourceImpl.EnterTripDataSourceImpl
 import com.going.data.datasourceImpl.MockDataSourceImpl
 import com.going.data.datasourceImpl.SettingDataSourceImpl
 import com.going.data.datasourceImpl.TodoDataSourceImpl
@@ -37,5 +39,10 @@ object DataSourceModule {
     @Singleton
     fun provideTodoDataSource(todoDataSourceImpl: TodoDataSourceImpl): TodoDataSource =
         todoDataSourceImpl
+
+    @Provides
+    @Singleton
+    fun provideEnterTripDataSource(entertripDataSourceImpl: EnterTripDataSourceImpl): EnterTripDataSource =
+        entertripDataSourceImpl
 
 }
--- app/src/main/java/com/going/doorip/di/RepositoryModule.kt
+++ app/src/main/java/com/going/doorip/di/RepositoryModule.kt
@@ -1,11 +1,13 @@
 package com.going.doorip.di
 
 import com.going.data.repositoryImpl.AuthRepositoryImpl
+import com.going.data.repositoryImpl.EnterTripRepositoryImpl
 import com.going.data.repositoryImpl.MockRepositoryImpl
 import com.going.data.repositoryImpl.SettingRepositoryImpl
 import com.going.data.repositoryImpl.TodoRepositoryImpl
 import com.going.data.repositoryImpl.TokenRepositoryImpl
 import com.going.domain.repository.AuthRepository
+import com.going.domain.repository.EnterTripRepository
 import com.going.domain.repository.MockRepository
 import com.going.domain.repository.SettingRepository
 import com.going.domain.repository.TodoRepository
@@ -44,5 +46,10 @@ object RepositoryModule {
     @Singleton
     fun provideTodoRepository(todoRepositoryImpl: TodoRepositoryImpl): TodoRepository =
         todoRepositoryImpl
+
+    @Provides
+    @Singleton
+    fun provideEnterTripRepository(entertripRepositoryImpl: EnterTripRepositoryImpl): EnterTripRepository =
+        entertripRepositoryImpl
 
 }
