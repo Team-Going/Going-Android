6d804ef80bcbc01b3b84cfa14b79e69d913bf925
crownjoe <crownjoe@naver.com>
2024-01-12 09:26:57 +0900

    [FEAT/#77] enter trip data 수정

--- data/src/main/java/com/going/data/dto/request/EnterTripRequestDto.kt
+++ data/src/main/java/com/going/data/dto/request/EnterTripRequestDto.kt
@@ -1,9 +1,15 @@
 package com.going.data.dto.request
 
+import com.going.domain.entity.request.RequestEnterTripModel
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class EnterTripRequestDto(
-
-
+    @SerialName("code")
+    val code: String,
 )
+
+fun RequestEnterTripModel.toEnterTripRequestDto(): EnterTripRequestDto =
+    EnterTripRequestDto(code)
+
