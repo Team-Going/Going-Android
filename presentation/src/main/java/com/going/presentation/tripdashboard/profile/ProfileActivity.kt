7c3f8176b2c86868259f79245d0c924b6eee57bc
crownjoe <crownjoe@naver.com>
2024-01-15 07:19:46 +0900

    [FEAT/#114] 프로필 뷰 서버통신 완료

--- presentation/src/main/java/com/going/presentation/tripdashboard/profile/ProfileActivity.kt
+++ presentation/src/main/java/com/going/presentation/tripdashboard/profile/ProfileActivity.kt
@@ -1,20 +1,38 @@
 package com.going.presentation.tripdashboard.profile
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.MediaScannerConnection
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.BulletSpan
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.going.presentation.R
+import com.going.presentation.dashboard.DashBoardActivity
 import com.going.presentation.databinding.ActivityProfileBinding
+import com.going.presentation.tendency.result.TendencyResultActivity.Companion.DOWNLOAD_IMAGE_NAME
+import com.going.presentation.tendency.result.TendencyResultActivity.Companion.DOWNLOAD_PATH
+import com.going.presentation.tendency.result.TendencyResultActivity.Companion.PERMISSION_REQUEST_CODE
+import com.going.presentation.tendency.splash.TendencySplashActivity
 import com.going.ui.base.BaseActivity
 import com.going.ui.extension.UiState
+import com.going.ui.extension.setOnSingleClickListener
 import com.going.ui.extension.toast
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import java.io.File
+import java.io.FileOutputStream
 
 @AndroidEntryPoint
 class ProfileActivity :
@@ -26,6 +44,9 @@ class ProfileActivity :
 
         getUserInfo()
         observeUserInfoState()
+        initRestartBtnClickListener()
+        initBackBtnClickListener()
+        initSaveImgBtnClickListener()
     }
 
     private fun getUserInfo() {
@@ -94,5 +115,115 @@ class ProfileActivity :
         string.setSpan(BulletSpan(10), 0, text.length - 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
 
         return string
+    }
+
+    private fun initRestartBtnClickListener() {
+        binding.tvProfileRestart.setOnSingleClickListener {
+            navigateToTendencySplashScreen()
+        }
+    }
+
+    private fun initSaveImgBtnClickListener() {
+        binding.btnProfileDownload.setOnSingleClickListener {
+            startImageDownload()
+        }
+    }
+
+    private fun initBackBtnClickListener() {
+        binding.btnProfileBack.setOnSingleClickListener {
+            navigateToDashBoardScreen()
+        }
+    }
+
+    private fun navigateToTendencySplashScreen() {
+        Intent(this, TendencySplashActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(this)
+        }
+        finish()
+    }
+
+    private fun navigateToDashBoardScreen() {
+        Intent(this, DashBoardActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(this)
+        }
+        finish()
+    }
+
+    private fun startImageDownload() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q &&
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                PERMISSION_REQUEST_CODE,
+            )
+        } else {
+            saveImageToGallery()
+        }
+    }
+
+    private fun saveImageToGallery() {
+        val imageBitmap: Bitmap = BitmapFactory.decodeResource(
+            resources,
+            R.drawable.img_tendency_result_ari,
+        )
+        val imageFileName = DOWNLOAD_IMAGE_NAME.replace(
+            "%s",
+            profileViewModel.profileId.value.toString()
+        )
+        val path = DOWNLOAD_PATH
+
+        val uploadFolder = getExternalFilesDir(path)
+        if (uploadFolder != null) {
+            if (!uploadFolder.exists()) {
+                uploadFolder?.mkdirs()
+            }
+        }
+
+        val imageFile = File(uploadFolder, imageFileName)
+
+        val outputStream = FileOutputStream(imageFile)
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        outputStream.flush()
+        outputStream.close()
+
+        MediaStore.Images.Media.insertImage(
+            contentResolver,
+            imageFile.absolutePath,
+            imageFileName,
+            null
+        )
+
+        toast(getString(R.string.profile_image_download_success))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startImageDownload()
+            } else {
+                toast(getString(R.string.profile_image_download_error))
+            }
+        }
+    }
+
+    private fun scanFile(file: File, mimeType: String) {
+        MediaScannerConnection.scanFile(
+            this,
+            arrayOf(file.absolutePath),
+            arrayOf(mimeType),
+            null,
+        )
     }
 }
