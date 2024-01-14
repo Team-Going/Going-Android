ebf2824f9ed1769e47b01c3e46452479b9287b40
LEE YOU BIN <youbeene3409@naver.com>
2024-01-14 22:58:07 +0900

    [FIX/#96] 코드 최적화

--- domain/src/main/kotlin/com/going/domain/entity/response/EnterPreferenceModel.kt
+++ domain/src/main/kotlin/com/going/domain/entity/response/EnterPreferenceModel.kt
@@ -1,10 +1,10 @@
 package com.going.domain.entity.response
 
 data class EnterPreferenceModel(
-    val tripId : Long,
-    val title : String,
-    val startDate : String,
-    val endDate : String,
-    val code : String,
-    val day : Int
+    val tripId: Long,
+    val title: String,
+    val startDate: String,
+    val endDate: String,
+    val code: String,
+    val day: Int
 )
--- presentation/src/main/java/com/going/presentation/preferencetag/entertrip/EnterPreferenceViewModel.kt
+++ presentation/src/main/java/com/going/presentation/preferencetag/entertrip/EnterPreferenceViewModel.kt
@@ -1,6 +1,5 @@
 package com.going.presentation.preferencetag.entertrip
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
