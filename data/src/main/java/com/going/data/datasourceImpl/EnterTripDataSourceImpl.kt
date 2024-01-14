a9bc8a7b5b5414e7812bc1ef3556606af114d165
crownjoe <crownjoe@naver.com>
2024-01-15 02:21:27 +0900

    [MOVE/#109] 패키징

--- data/src/main/java/com/going/data/datasourceImpl/EnterTripDataSourceImpl.kt
+++ data/src/main/java/com/going/data/datasourceImpl/EnterTripDataSourceImpl.kt
@@ -3,7 +3,9 @@ package com.going.data.datasourceImpl
 import com.going.data.datasource.EnterTripDataSource
 import com.going.data.dto.BaseResponse
 import com.going.data.dto.request.EnterTripRequestDto
+import com.going.data.dto.request.StartInviteTripRequestDto
 import com.going.data.dto.response.EnterTripResponseDto
+import com.going.data.dto.response.StartInviteTripResponseDto
 import com.going.data.service.EnterTripService
 import javax.inject.Inject
 
@@ -14,4 +16,10 @@ class EnterTripDataSourceImpl @Inject constructor(
         code: EnterTripRequestDto
     ): BaseResponse<EnterTripResponseDto> =
         enterTripService.postEnterTrip(code)
+
+    override suspend fun postStartInviteTrip(
+        tripId: Long,
+        request: StartInviteTripRequestDto
+    ): BaseResponse<StartInviteTripResponseDto> =
+        EnterTripService.postStartInviteTrip(tripId, request)
 }
