a30d3aee82ed984bd8b056b946b22482e25e22a8
Dongmin <pdm001125@naver.com>
2024-01-16 00:31:16 +0900

    [refactor/#129] 이모지 글자 포함 글자 수 세는 함수 extention으로 변경

--- core-ui/src/main/java/com/going/ui/extension/StringExt.kt
+++ core-ui/src/main/java/com/going/ui/extension/StringExt.kt
@@ -1,5 +1,20 @@
 package com.going.ui.extension
 
+import java.text.BreakIterator
+
 fun String?.isJsonObject(): Boolean = this?.startsWith("{") == true && this.endsWith("}")
 
 fun String?.isJsonArray(): Boolean = this?.startsWith("[") == true && this.endsWith("]")
+
+fun String.getGraphemeLength(): Int {
+    val breakIterator: BreakIterator = BreakIterator.getCharacterInstance()
+
+    breakIterator.setText(this)
+
+    var count = 0
+    while (breakIterator.next() != BreakIterator.DONE) {
+        count++
+    }
+
+    return count
+}
