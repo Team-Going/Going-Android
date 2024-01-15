2ea2f16740b6370a42d06b133095bd07fe0ea8db
Sangho Kim <marchbreeze03@gmail.com>
2024-01-16 06:45:54 +0900

    [FIX/#123] data 모듈 스타일 통일

--- data/src/main/java/com/going/data/repositoryImpl/SettingRepositoryImpl.kt
+++ data/src/main/java/com/going/data/repositoryImpl/SettingRepositoryImpl.kt
@@ -7,11 +7,13 @@ import javax.inject.Inject
 class SettingRepositoryImpl @Inject constructor(
     private val settingDataSource: SettingDataSource,
 ) : SettingRepository {
-    override suspend fun patchSignOut(): Result<Unit> = runCatching {
-        settingDataSource.patchSignOut()
-    }
+    override suspend fun patchSignOut(): Result<Unit> =
+        runCatching {
+            settingDataSource.patchSignOut()
+        }
 
-    override suspend fun deleteWithDraw(): Result<Unit> = runCatching {
-        settingDataSource.deleteWithDraw()
-    }
+    override suspend fun deleteWithDraw(): Result<Unit> =
+        runCatching {
+            settingDataSource.deleteWithDraw()
+        }
 }
