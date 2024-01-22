bf85429ea8087db85a5cb5c8afa40b7b52510dfd
Dongmin <pdm001125@naver.com>
2024-01-22 17:40:14 +0900

    [FEAT/#184] custom edittext class 구현

---
+++ presentation/src/main/java/com/going/presentation/designsystem/edittext/EmojiCounterEditText.kt
@@ -0,0 +1,27 @@
+package com.going.presentation.designsystem.edittext
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.going.presentation.R
+import com.going.presentation.databinding.ViewEmojiCounterEdittextBinding
+
+class EmojiCounterEditText(context: Context, attrs: AttributeSet) :
+    ConstraintLayout(context, attrs) {
+    private var binding: ViewEmojiCounterEdittextBinding = ViewEmojiCounterEdittextBinding.inflate(
+        LayoutInflater.from(context),
+        this,
+        true,
+    )
+
+    init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.EmojiCounterEditText)
+
+        binding.tvEmojiCounterEtTitle.text = typedArray.getString(R.styleable.EmojiCounterEditText_title)
+        binding.etEmojiCounterEtContent.setText(typedArray.getString(R.styleable.EmojiCounterEditText_text))
+        binding.etEmojiCounterEtContent.hint = typedArray.getString(R.styleable.EmojiCounterEditText_hint)
+        binding.tvEmojiCounterEtWarningMessage.text = typedArray.getString(R.styleable.EmojiCounterEditText_warning)
+        binding.tvEmojiCounterEtNameCounter.text = typedArray.getString(R.styleable.EmojiCounterEditText_count)
+    }
+}
