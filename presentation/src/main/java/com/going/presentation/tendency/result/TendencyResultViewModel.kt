3b6be94edcd918112068297bf4e6bf77c8662f29
Dongmin <pdm001125@naver.com>
2024-02-04 21:47:48 +0900

    [FIX/#195] LiveData -> StateFlow 초기값 지정하여 null체크 미사용

--- presentation/src/main/java/com/going/presentation/tendency/result/TendencyResultViewModel.kt
+++ presentation/src/main/java/com/going/presentation/tendency/result/TendencyResultViewModel.kt
@@ -1,6 +1,5 @@
 package com.going.presentation.tendency.result
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.going.domain.entity.request.UserProfileRequestModel
@@ -20,7 +19,7 @@ class TendencyResultViewModel @Inject constructor(
     private val _userInfoState = MutableStateFlow<UiState<UserProfileRequestModel>>(UiState.Empty)
     val userInfoState: StateFlow<UiState<UserProfileRequestModel>> = _userInfoState
 
-    val tendencyId = MutableLiveData(0)
+    val tendencyId = MutableStateFlow(0)
 
     fun getUserInfoState() {
         viewModelScope.launch {
