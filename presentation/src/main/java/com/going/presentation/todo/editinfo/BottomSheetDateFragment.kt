59129a5b8daaedcb9aa9136ed75b0bad751a88cb
crownjoe <crownjoe@naver.com>
2024-02-28 02:27:57 +0900

    [FEAT/#212] 데이트피커 ui 구현

---
+++ presentation/src/main/java/com/going/presentation/todo/editinfo/BottomSheetDateFragment.kt
@@ -0,0 +1,112 @@
+package com.going.presentation.todo.editinfo
+
+import android.os.Bundle
+import android.view.View
+import com.going.presentation.R
+import com.going.presentation.databinding.FragmentBottomSheetEditDateTripBinding
+import com.going.ui.base.BaseBottomSheet
+import com.going.ui.extension.setOnSingleClickListener
+import com.going.ui.extension.toast
+import java.util.Calendar
+
+class BottomSheetDateFragment(val viewModel: EditTripInfoViewModel, val isStart: Boolean) :
+    BaseBottomSheet<FragmentBottomSheetEditDateTripBinding>(R.layout.fragment_bottom_sheet_edit_date_trip) {
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setBackgroundDrawableResource(R.color.transparent)
+        if (isStart) customStartDate() else customEndDate()
+        observeIsAvailableDateRange()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        initFinishBtnClickListener()
+    }
+
+    private fun customStartDate() {
+        val datePicker = binding.dpEditTripDate
+        val calendar = Calendar.getInstance()
+
+        val currentStartYear = calendar.get(Calendar.YEAR)
+        val currentStartMonth = calendar.get(Calendar.MONTH)
+        val currentStartDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        calendar.set(2000, 0, 1)
+        datePicker.minDate = calendar.timeInMillis
+
+        calendar.set(2100, 0, 1)
+        datePicker.maxDate = calendar.timeInMillis
+
+        datePicker.updateDate(currentStartYear, currentStartMonth, currentStartDay)
+    }
+
+    private fun customEndDate() {
+        val datePicker = binding.dpEditTripDate
+        val calendar = Calendar.getInstance()
+
+        val currentEndYear = viewModel.startYear.value ?: 0
+        val currentEndMonth = viewModel.startMonth.value ?: 0
+        val currentEndDay = viewModel.startDay.value ?: 0
+
+        calendar.set(currentEndYear, currentEndMonth - 1, currentEndDay)
+        datePicker.minDate = calendar.timeInMillis
+
+        calendar.set(2100, 0, 1)
+        datePicker.maxDate = calendar.timeInMillis
+    }
+
+    private fun observeIsAvailableDateRange() {
+        viewModel.isAvailableDateRange.observe(this) {
+            when (it) {
+                null -> return@observe
+                true -> return@observe
+                false -> toast(getString(R.string.date_set_error))
+            }
+        }
+    }
+
+    private fun sendDateInfo() {
+        if (isStart) {
+            viewModel.startYear.value = binding.dpEditTripDate.year
+            viewModel.startMonth.value = binding.dpEditTripDate.month + 1
+            viewModel.startDay.value = binding.dpEditTripDate.dayOfMonth
+            viewModel.checkStartDateAvailable()
+        } else {
+            customEndDate()
+            viewModel.endYear.value = binding.dpEditTripDate.year
+            viewModel.endMonth.value = binding.dpEditTripDate.month + 1
+            viewModel.endDay.value = binding.dpEditTripDate.dayOfMonth
+            viewModel.checkEndDateAvailable()
+        }
+    }
+
+    private fun initFinishBtnClickListener() {
+        binding.btnEditTripSelect.setOnSingleClickListener {
+            sendDateInfo()
+            if (viewModel.isStartDateAvailable.value == true && viewModel.isEndDateAvailable.value == true) {
+                val calendar = Calendar.getInstance()
+
+                calendar.set(Calendar.YEAR, viewModel.startYear.value ?: 0)
+                calendar.set(Calendar.MONTH, viewModel.startMonth.value ?: 0)
+                calendar.set(Calendar.DAY_OF_MONTH, viewModel.startDay.value ?: 0)
+                val startDate = calendar.time
+
+                calendar.set(Calendar.YEAR, viewModel.endYear.value ?: 0)
+                calendar.set(Calendar.MONTH, viewModel.endMonth.value ?: 0)
+                calendar.set(Calendar.DAY_OF_MONTH, viewModel.endDay.value ?: 0)
+                val endDate = calendar.time
+
+                if (startDate.before(endDate) || startDate.equals(endDate)) {
+                    viewModel.checkStartDateAvailable()
+                    viewModel.checkEndDateAvailable()
+                }
+                viewModel.checkIsAvailableDateRange()
+            }
+            viewModel.checkTripAvailable()
+            dismiss()
+        }
+    }
+}
+
