34d3701f0a34be1caab2ef4ec1f9995cff7e600a
Dongmin <pdm001125@naver.com>
2024-03-03 16:28:29 +0900

    [ADD/#223] ViewPagerAdapter 및 해당 fragment 추가

---
+++ presentation/src/main/java/com/going/presentation/profile/trip/TripProfileCharacterFragment.kt
@@ -0,0 +1,9 @@
+package com.going.presentation.profile.trip
+
+import com.going.presentation.R
+import com.going.presentation.databinding.FragmentTripProfileCharacterBinding
+import com.going.ui.base.BaseFragment
+
+class TripProfileCharacterFragment :
+    BaseFragment<FragmentTripProfileCharacterBinding>(R.layout.fragment_trip_profile_character) {
+}
---
+++ presentation/src/main/java/com/going/presentation/profile/trip/TripProfileTagFragment.kt
@@ -0,0 +1,8 @@
+package com.going.presentation.profile.trip
+
+import com.going.presentation.R
+import com.going.presentation.databinding.FragmentTripProfileTagBinding
+import com.going.ui.base.BaseFragment
+
+class TripProfileTagFragment: BaseFragment<FragmentTripProfileTagBinding>(R.layout.fragment_trip_profile_tag) {
+}
