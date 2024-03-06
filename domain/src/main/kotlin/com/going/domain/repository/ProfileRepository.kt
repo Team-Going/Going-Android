5ecbb34b83700fc92a33e8eb169fb79d9e78751e
Dongmin <pdm001125@naver.com>
2024-03-06 17:59:37 +0900

    [ADD/#233] domain request 관련 정의

--- domain/src/main/kotlin/com/going/domain/repository/ProfileRepository.kt
+++ domain/src/main/kotlin/com/going/domain/repository/ProfileRepository.kt
@@ -1,7 +1,10 @@
 package com.going.domain.repository
 
+import com.going.domain.entity.request.ParticipantProfileRequestModel
 import com.going.domain.entity.request.UserProfileRequestModel
 
 interface ProfileRepository {
     suspend fun getUserProfile(): Result<UserProfileRequestModel>
+
+    suspend fun getParticipantProfile(): Result<ParticipantProfileRequestModel>
 }
