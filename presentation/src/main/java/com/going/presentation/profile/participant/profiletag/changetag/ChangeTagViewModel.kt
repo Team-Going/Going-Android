ab763c738ee297c0245368737369a8aad7cee70c
LEE YOU BIN <youbeene3409@naver.com>
2024-03-14 00:50:45 +0900

    [FIX/#257] 취향 태그 버튼 활성화 로직 수정

--- presentation/src/main/java/com/going/presentation/profile/participant/profiletag/changetag/ChangeTagViewModel.kt
+++ presentation/src/main/java/com/going/presentation/profile/participant/profiletag/changetag/ChangeTagViewModel.kt
@@ -8,7 +8,9 @@ import com.going.domain.entity.request.PreferenceChangeRequestModel
 import com.going.domain.repository.ProfileRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -20,13 +22,80 @@ class ChangeTagViewModel @Inject constructor(
     private val _preferencePatchState = MutableSharedFlow<Boolean>()
     val preferencePatchState: SharedFlow<Boolean> = _preferencePatchState
 
+    private val _isButtonValid = MutableStateFlow(false)
+    val isButtonValid: StateFlow<Boolean> = _isButtonValid
+
     var tripId: Long = 0
 
-    val styleA = MutableLiveData(0)
-    val styleB = MutableLiveData(0)
-    val styleC = MutableLiveData(0)
-    val styleD = MutableLiveData(0)
-    val styleE = MutableLiveData(0)
+    private val defaultStyleA = MutableLiveData(0)
+    private val defaultStyleB = MutableLiveData(0)
+    private val defaultStyleC = MutableLiveData(0)
+    private val defaultStyleD = MutableLiveData(0)
+    private val defaultStyleE = MutableLiveData(0)
+
+    private val styleA = MutableLiveData(0)
+    private val styleB = MutableLiveData(0)
+    private val styleC = MutableLiveData(0)
+    private val styleD = MutableLiveData(0)
+    private val styleE = MutableLiveData(0)
+
+    private var isStyleAChanged: Boolean = false
+    private var isStyleBChanged: Boolean = false
+    private var isStyleCChanged: Boolean = false
+    private var isStyleDChanged: Boolean = false
+    private var isStyleEChanged: Boolean = false
+
+    fun setDefaultPreference(styleA: Int, styleB: Int, styleC: Int, styleD: Int, styleE: Int) {
+        defaultStyleA.value = styleA
+        this.styleA.value = styleA
+
+        defaultStyleB.value = styleB
+        this.styleB.value = styleB
+
+        defaultStyleC.value = styleC
+        this.styleC.value = styleC
+
+        defaultStyleD.value = styleD
+        this.styleD.value = styleD
+
+        defaultStyleE.value = styleE
+        this.styleE.value = styleE
+    }
+
+    fun checkIsPreferenceChange(number: Int, index: Int) {
+        when (number) {
+            1 -> {
+                styleA.value = index
+                isStyleAChanged = index != defaultStyleA.value
+            }
+
+            2 -> {
+                styleB.value = index
+                isStyleBChanged = index != defaultStyleB.value
+            }
+
+            3 -> {
+                styleC.value = index
+                isStyleCChanged = index != defaultStyleC.value
+            }
+
+            4 -> {
+                styleD.value = index
+                isStyleDChanged = index != defaultStyleD.value
+            }
+
+            5 -> {
+                styleE.value = index
+                isStyleEChanged = index != defaultStyleE.value
+            }
+        }
+        checkIsButtonValid()
+    }
+
+    private fun checkIsButtonValid() {
+        _isButtonValid.value =
+            isStyleAChanged || isStyleBChanged || isStyleCChanged || isStyleDChanged || isStyleEChanged
+    }
 
     fun patchPreferenceTagToServer() {
         viewModelScope.launch {
